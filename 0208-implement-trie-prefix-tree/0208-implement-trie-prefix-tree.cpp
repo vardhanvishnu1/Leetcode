bf9f0fb2422d6f1b012d841ebed6b5f8d909e15f
vardhanvishnu1 <vardhanbharathula@gmail.com>
2025-11-11 14:31:43 +0530

    Time: 16 ms (82.57%), Space: 50.5 MB (52.6%) - LeetHub

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class TrieNode {
+public:
+    bool isEnd;
+    TrieNode* children[26];
+    TrieNode() {
+        isEnd = false;
+        for (int i = 0; i < 26; i++)
+            children[i] = nullptr;
+    }
+};
+
+class Trie {
+private:
+    TrieNode* root;
+public:
+    Trie() {
+        root = new TrieNode();
+    }
+
+    void insert(string word) {
+        TrieNode* node = root;
+        for (char c : word) {
+            int idx = c - 'a';
+            if (node->children[idx] == nullptr)
+                node->children[idx] = new TrieNode();
+            node = node->children[idx];
+        }
+        node->isEnd = true;
+    }
+
+    bool search(string word) {
+        TrieNode* node = root;
+        for (char c : word) {
+            int idx = c - 'a';
+            if (node->children[idx] == nullptr)
+                return false;
+            node = node->children[idx];
+        }
+        return node->isEnd;
+    }
+
+    bool startsWith(string prefix) {
+        TrieNode* node = root;
+        for (char c : prefix) {
+            int idx = c - 'a';
+            if (node->children[idx] == nullptr)
+                return false;
+            node = node->children[idx];
+        }
+        return true;
+    }
+};
