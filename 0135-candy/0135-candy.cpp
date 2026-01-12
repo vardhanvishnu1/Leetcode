99e3f74e9b81fdac9eb25b726dc7dbb01b750d52
vardhanvishnu1 <vardhanbharathula@gmail.com>
2026-01-12 23:01:49 +0530

    Time: 0 ms (100%), Space: 22.9 MB (70.29%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -12,20 +12,17 @@ public:
                 left[i] = 1;
             }
         }
-        vector<int>right(n,0);
-        right[n-1] = 1;
+        int ans =0;
+        int curr = 1;int right = 1;
         for(int i=n-2;i>=0;i--){
             if(ratings[i]>ratings[i+1]){
-                right[i] = right[i+1]+1;
+                curr+=1;
             }
             else{
-                right[i] = 1;
+                curr = 1;
             }
+            ans+=max(left[i],curr);
         }
-        int ans =0;
-        for(int i=0;i<n;i++){
-            ans+=max(left[i],right[i]);
-        }
-        return ans;
+        return ans+max(left[n-1],1);
     }
 };
