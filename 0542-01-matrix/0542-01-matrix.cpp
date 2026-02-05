72fcf27ec80d9ab862789908dc906401a1fe9391
vardhanvishnu1 <vardhanbharathula@gmail.com>
2026-02-06 00:02:37 +0530

    Time: 8 ms (90.32%), Space: 34.8 MB (74.58%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,39 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int n = mat.size(), m = mat[0].size();
-        vector<vector<int>> dist(n, vector<int>(m, -1));
-        queue<pair<int, int>> q;
-
-        // Step 1: Push all 0s into queue and mark distance 0
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (mat[i][j] == 0) {
+        int m  = mat.size();
+        int n = mat[0].size();
+        queue<pair<int,int>>q;
+        vector<vector<int>>dist(m,vector<int>(n,-1));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j]==0){
                     dist[i][j] = 0;
-                    q.push({i, j});
+                    q.push({i,j});
                 }
             }
         }
 
-        // Step 2: Directions array
-        vector<vector<int>> dir = {{0,1}, {0,-1}, {1,0}, {-1,0}};
-
-        // Step 3: BFS traversal
-        while (!q.empty()) {
-            auto [r, c] = q.front();
-            q.pop();
-
-            for (auto& d : dir) {
-                int nr = r + d[0];
-                int nc = c + d[1];
-                if (nr >= 0 && nc >= 0 && nr < n && nc < m && dist[nr][nc] == -1) {
-                    dist[nr][nc] = dist[r][c] + 1;
-                    q.push({nr, nc});
-                }
-            }
+    vector<vector<int>>dir = {{1,0},{-1,0},{0,1},{0,-1}};
+    while(!q.empty()){
+        auto [curr_i,curr_j] = q.front();q.pop();
+        for(int d = 0;d<4;d++){
+                int new_i = curr_i + dir[d][0];
+                int new_j = curr_j + dir[d][1];
+                if(new_i<0||new_j<0||new_i>=m||new_j>=n||dist[new_i][new_j]!=-1) continue;
+                dist[new_i][new_j] = dist[curr_i][curr_j] +1;
+                q.push({new_i,new_j});
         }
-
-        return dist;
+    }
+    return dist;
     }
 };
-
