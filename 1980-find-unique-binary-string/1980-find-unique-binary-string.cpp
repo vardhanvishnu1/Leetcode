73f2d1973cf6d41a3a9371106ec3c9d940aab7f3
vardhanvishnu1 <vardhanbharathula@gmail.com>
2026-03-08 13:25:24 +0530

    Time: 1 ms (27.83%), Space: 14.2 MB (17.15%) - LeetHub

---
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string generate(int sz,int mx_sz,string curr,set<string>&s){
+        // if(sz>mx_sz) return "";
+        if(sz==mx_sz){
+            if(s.find(curr)==s.end()) return curr;
+            return "";
+        }
+        string x = generate(sz+1,mx_sz,curr+"0",s);
+        if(x!="") return x;
+        x = generate(sz+1,mx_sz,curr+"1",s);
+        if(x!="") return x;
+        return "";
+    }
+    string findDifferentBinaryString(vector<string>& nums) {
+        set<string>s;
+        int mx_sz = nums[0].size();
+        for(auto el :nums){
+            s.insert(el);
+        }
+        return generate(0,mx_sz,"",s);
+    }
+};
