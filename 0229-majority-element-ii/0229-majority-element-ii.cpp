2a7fb70a0bbfe57bc776ca499681928723963692
vardhanvishnu1 <vardhanbharathula@gmail.com>
2026-03-14 21:19:32 +0530

    Time: 0 ms (100%), Space: 23.5 MB (83.25%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -7,20 +7,18 @@ public:
         int can1 = -1;
         int can2 = -1;
         for(int i=0;i<n;i++){
-          if(nums[i]==can1&&nums[i]!=can2){
-            cnt1++;
-          }
-          else if(nums[i]==can2&&nums[i]!=can1){
-            cnt2++;
-          }
-          else if(cnt1==0){
+          if(cnt1==0&&nums[i]!=can2){
             can1 = nums[i];
             cnt1++;
           }
-          else if(cnt2==0){
+        else if(cnt2==0&&nums[i]!=can1){
             can2 = nums[i];
             cnt2++;
           }
+          else if(nums[i]==can1){
+            cnt1++;
+          }
+          else if(nums[i]==can2) cnt2++;
           else{
             cnt1--;cnt2--;
           }
