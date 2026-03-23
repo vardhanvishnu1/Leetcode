ce57be0817ae5807deca753a3d31cd20913d4c3a
vardhanvishnu1 <vardhanbharathula@gmail.com>
2026-03-24 01:23:40 +0530

    Time: 7 ms (19.76%), Space: 9.7 MB (73.82%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size()!=t.size()) return false;
-        map<char,int>mp1;
-        map<char,int>mp2;
-        for(int i=0;i<s.size();i++){
-            mp1[s[i]]++;
-        }
-        for(int i=0;i<t.size();i++){
-            mp2[t[i]]++;
-        }
-        for(auto el : mp1){
-            if(el.second!=mp2[el.first]) return false;
-        }
-        return true;
+        // 1st approach 
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        return s==t;
+        // 2nd approach
+        // if(s.size()!=t.size()) return false;
+        // map<char,int>mp1;
+        // map<char,int>mp2;
+        // for(int i=0;i<s.size();i++){
+        //     mp1[s[i]]++;
+        // }
+        // for(int i=0;i<t.size();i++){
+        //     mp2[t[i]]++;
+        // }
+        // for(auto el : mp1){
+        //     if(el.second!=mp2[el.first]) return false;
+        // }
+        // return true;
     }
 };
