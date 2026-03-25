2f0ed5514add2ebfc2c536f1d11997847e9376f1
vardhanvishnu1 <vardhanbharathula@gmail.com>
2026-03-25 19:08:19 +0530

    Time: 52 ms (19.29%), Space: 75.7 MB (28.11%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.cpp
@@ -0,0 +1,45 @@
+
+class Solution {
+public:
+      ListNode* mergeTwoLists(ListNode* a, ListNode* b) {
+         ListNode* c = new  ListNode(100);
+         ListNode* temp = c;
+         while(a!=NULL && b!=NULL){
+            if(a->val<=b->val){
+                temp->next = a;
+                a = a->next;
+                temp = temp->next;
+            }
+            else{
+                temp->next = b;
+                b = b->next;
+                temp = temp->next;
+            }
+            
+         }
+          if(a==NULL){
+                temp->next = b;
+            }
+            else{
+                temp->next = a;
+            }
+         return c->next;
+    }
+    ListNode* sortList(ListNode* head) {
+        if(head==NULL || head->next==NULL) return head;
+      ListNode* slow = head;
+      ListNode* fast = head;
+      while(fast->next!=NULL && fast->next->next !=NULL){
+        slow = slow->next;
+        fast = fast->next->next;
+      }
+        ListNode* a= head;
+        ListNode* b =  slow->next;
+        slow->next = NULL;
+        a = sortList(a);
+        b = sortList(b);
+        ListNode* ans = mergeTwoLists(a,b);
+      
+      return ans;
+    }
+};
