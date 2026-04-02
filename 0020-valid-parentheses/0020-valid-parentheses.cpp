b2fb1a4385daaaff3dbed8ff9d3ab0b2b4594417
vardhanvishnu1 <vardhanbharathula@gmail.com>
2026-04-03 00:27:00 +0530

    Time: 0 ms (100%), Space: 8.9 MB (38.67%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,27 +1,17 @@
 class Solution {
 public:
     bool isValid(string s) {
-        int count1 = 0;
-        int count2 =0;
         stack<char>st;
-        if(s.size()&1) return false;
         for(int i=0;i<s.size();i++){
-            if(s[i]=='('||s[i]=='['||s[i]=='{') st.push(s[i]);
-            if(st.empty()&&(s[i]==')'||s[i]==']'||s[i]=='}')) return false;
-            else if(s[i]==')'){
-                if(st.top()!='(') return false;
-                st.pop();
+            if(s[i]=='{'||s[i]=='['||s[i]=='('){
+                st.push(s[i]);
             }
-            else if(s[i]==']'){
-                if(st.top()!='[') return false;
-                st.pop();
-            }
-            else if(s[i]=='}'){
-                if(st.top()!='{') return false;
-                st.pop();
+            else{
+                if(st.empty()) return false;
+                if((s[i]==')'&&st.top()=='(')||(s[i]==']'&&st.top()=='[')||(s[i]=='}'&&st.top()=='{')) st.pop();
+                else return false;
             }
         }
-        if(!st.empty()) return false;
-        return true;
+        return st.empty();
     }
 };
