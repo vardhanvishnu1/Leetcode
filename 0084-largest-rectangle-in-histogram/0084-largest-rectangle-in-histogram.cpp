36dc68d1af437fb57fdefc1995dda116b099d990
vardhanvishnu1 <vardhanbharathula@gmail.com>
2026-04-05 00:30:42 +0530

    Time: 16 ms (81.28%), Space: 85.6 MB (41.62%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -6,6 +6,7 @@ public:
         vector<int>nse(n);
         vector<int>pse(n);
         stack<int>st;
+        // next Smaller Element (right)
         for(int i=n-1;i>=0;i--){
             if(st.empty()) {st.push(i);nse[i]=n;continue;}
             while(!st.empty()&&heights[st.top()]>=heights[i]){
@@ -24,6 +25,7 @@ public:
             pse[i] = st.empty() ? -1 : st.top();
             st.push(i);
         }
+        //The code calculates answer based on both height and width. It doesn't pick one over the other based on height; it calculates the maximum area for every height and then uses max() to see which strategy won.
         int ans = 0;
          for (int i = 0; i < n; i++) {
             int width = nse[i] - pse[i] - 1;
