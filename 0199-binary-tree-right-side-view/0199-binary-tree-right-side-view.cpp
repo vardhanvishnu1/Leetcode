749c3bb7a1406dcf25f2a156bc52230099e4d0ba
vardhanvishnu1 <vardhanbharathula@gmail.com>
2026-06-13 00:40:58 +0530

    Time: 0 ms (100%), Space: 15.1 MB (41.36%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -1,37 +1,27 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    void levelOrderTraversal(TreeNode* root,vector<int>&ans){
-        queue<TreeNode*>q;
-        q.push(root);             
-        while(!q.empty()){
-            vector<int>level;
-            int size = q.size();
-            for(int i=0;i<size;i++){
-                TreeNode* curr = q.front();
-                q.pop();
-                if(i==size-1) level.push_back(curr->val);
-                if(curr->left) q.push(curr->left);
-                if(curr->right) q.push(curr->right);
-            }
-            ans.push_back(level[0]);
-        }
-        return;
-    }
     vector<int> rightSideView(TreeNode* root) {
-        if(!root) return {};
-        vector<int>ans;
-        levelOrderTraversal(root,ans);
+        vector<int> ans;
+        if (!root) return ans;
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            int sz = q.size();
+
+            for (int i = 0; i < sz; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+
+                // first node at level = rightmost
+                if (i == 0) ans.push_back(node->val);
+
+                if (node->right) q.push(node->right);
+                if (node->left) q.push(node->left);
+            }
+        }
+
         return ans;
     }
 };
