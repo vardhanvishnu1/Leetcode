38ca95791ec4c3c2fb89dec850f59831f7526618
vardhanvishnu1 <vardhanbharathula@gmail.com>
2026-06-27 15:57:28 +0530

    Time: 13 ms (95.08%), Space: 17.9 MB (71.85%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
- int solve(int n,int sum,int idx,vector<int>& coins, int amount,vector<vector<int>>&dp){
-    if(sum==amount) return 0;
-    if(sum>amount||idx==n) return INT_MAX;
-    if(dp[sum][idx]!=-1) return dp[sum][idx];
-    int take = INT_MAX;
-    if (coins[idx] <= amount - sum){
-    int res = solve(n,sum+coins[idx],idx,coins,amount,dp);
-    if(res==INT_MAX) take = INT_MAX;
-    else take = 1+res;
-    }
-    int skip = solve(n,sum,idx+1,coins,amount,dp);
-    return dp[sum][idx] = min(take,skip);
-}
     int coinChange(vector<int>& coins, int amount) {
-        int n = coins.size();
-        vector<vector<int>>dp(amount+1,vector<int>(n,-1));
-        int x =  (int)solve(n,0,0,coins,amount,dp);
-        if(x==INT_MAX) return -1;
-        return x;
+
+        const int INF = 1e9;
+        vector<int> dp(amount + 1, INF);
+
+        dp[0] = 0;
+
+        for(int coin : coins)
+        {
+            for(int x = coin; x <= amount; x++)
+            {
+                dp[x] = min(dp[x], dp[x - coin] + 1);
+            }
+        }
+
+        return dp[amount] == INF ? -1 : dp[amount];
     }
 };
