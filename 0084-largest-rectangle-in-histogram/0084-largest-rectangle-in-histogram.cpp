edb840688e97004f6b67475444185f0efbefb599
vardhanvishnu1 <vardhanbharathula@gmail.com>
2026-07-07 15:13:46 +0530

    Time: 32 ms (22.71%), Space: 85.7 MB (35.12%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,35 +1,36 @@
 class Solution {
 public:
-
     int largestRectangleArea(vector<int>& heights) {
-          int n = heights.size();
-        vector<int>nse(n);
-        vector<int>pse(n);
+        int n = heights.size();
+        int ans = 0;
         stack<int>st;
-        // next Smaller Element (right)
+        vector<int>nse(n);
         for(int i=n-1;i>=0;i--){
-            if(st.empty()) {st.push(i);nse[i]=n;continue;}
-            while(!st.empty()&&heights[st.top()]>=heights[i]){
+            if(st.empty()){
+                st.push(i);nse[i] = n;continue;
+            }
+            while(!st.empty()&&heights[i]<=heights[st.top()]){
                 st.pop();
             }
             if(st.empty()) nse[i] = n;
             else nse[i] = st.top();
             st.push(i);
         }
-         while (!st.empty()) st.pop();
-
-        // Previous Smaller Element (Left)
-        for (int i = 0; i < n; i++) {
-            while (!st.empty() && heights[st.top()] >= heights[i])
+        while(!st.empty()) st.pop();
+        vector<int>pse(n);
+        for(int i=0;i<n;i++){
+            if(st.empty()){
+                st.push(i);pse[i] = -1;continue;
+            }
+            while(!st.empty()&&heights[i]<=heights[st.top()]){
                 st.pop();
-            pse[i] = st.empty() ? -1 : st.top();
+            }
+            if(st.empty()) pse[i] = -1;
+            else pse[i] = st.top();
             st.push(i);
         }
-        //The code calculates answer based on both height and width. It doesn't pick one over the other based on height; it calculates the maximum area for every height and then uses max() to see which strategy won.
-        int ans = 0;
-         for (int i = 0; i < n; i++) {
-            int width = nse[i] - pse[i] - 1;
-            ans = max(ans, width * heights[i]);
+        for(int i=0;i<n;i++){
+            ans = max(ans,(nse[i]-pse[i]-1)*(heights[i]));
         }
         return ans;
     }
