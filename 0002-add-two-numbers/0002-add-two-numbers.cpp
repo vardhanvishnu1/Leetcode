4887a7304b85893ba8c13ce548061838a742e41a
vardhanvishnu1 <vardhanbharathula@gmail.com>
2026-07-08 16:15:00 +0530

    Time: 0 ms (100%), Space: 76.5 MB (99.3%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -13,28 +13,24 @@ public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         ListNode* t1 = l1;
         ListNode* t2 = l2;
+        int carry = 0;
         ListNode* prev = NULL;
-        bool flag = false;
         while(t1||t2){
-            int curr =0;
-            if(t1) curr+=t1->val;
-            if(t2) curr+=t2->val;
-            if(flag){curr++;flag = false;}
-            if(t1){
-                t1->val = curr%10;
-            }
-            else{
-                prev->next = new ListNode(curr%10);
-                t1 = prev->next;
-            }
-            if(curr>9) flag = true;
+            int c1 =0,c2 =0;
+            if(t1) c1 = t1->val;
+            if(t2) c2 = t2->val;
+            int total = c1+c2+carry;
+            if(carry==1) carry = 0;
+            int cur = 0;
+            if(total>9) {carry = 1;cur = total%10;}
+            else cur = total;
+            if(t1) t1->val = cur;
+            else {prev->next = new ListNode(cur);t1 = prev->next;}
             prev = t1;
             if(t1) t1 = t1->next;
             if(t2) t2 = t2->next;
         }
-        if(flag){
-            prev->next = new ListNode(1);
-        }
+        if(carry==1&&prev) prev->next = new ListNode(1);
         return l1;
     }
 };
