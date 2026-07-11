5304d83e7083aca58fa20a215c0c73097da1566d
vardhanvishnu1 <vardhanbharathula@gmail.com>
2026-07-11 19:10:10 +0530

    Time: 14 ms (21.15%), Space: 11.1 MB (19.41%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -3,17 +3,15 @@ public:
     int characterReplacement(string s, int k) {
         int n = s.size();
         int l =0;
-        int r = 0;
+        int r =0;
+        int mf = 0;
         int ans = 0;
-        int max_freq = 0;
         map<int,int>freq;
         while(r<n){
             freq[s[r]-'A']++;
-            max_freq = max(max_freq,freq[s[r]-'A']);
-            int len = r-l+1;
-            while(r-l+1-max_freq>k){
+            mf = max(mf,freq[s[r]-'A']);
+            while((r-l+1)-mf>k){
                 freq[s[l]-'A']--;
-                if(freq[s[l]-'A']==0) freq.erase(s[l]-'A');
                 l++;
             }
             ans = max(ans,r-l+1);
