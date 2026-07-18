eb516cfeb4ab414a10a706265f40f3f843717187
vardhanvishnu1 <vardhanbharathula@gmail.com>
2026-07-19 01:04:26 +0530

    Time: 3 ms (12.82%), Space: 18.6 MB (8.5%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -4,26 +4,29 @@ public:
         int m = image.size();
         int n = image[0].size();
         int org = image[sr][sc];
-        queue<tuple<int,int,int>>q;
-        vector<vector<int>>dir = {{1,0},{-1,0},{0,1},{0,-1}};
-        q.push({sr,sc,org});
-        vector<vector<bool>>visited(m,vector<bool>(n,false));
-        visited[sr][sc] = true;
-        image[sr][sc] = color;
-        while(!q.empty()){
-            auto [ci,cj,x] = q.front();
-            org = x;
-            q.pop();
-            for(int d=0;d<4;d++){
-                int ni = ci + dir[d][0];
-                int nj = cj + dir[d][1];
-                if(ni<0||nj<0||ni>=m||nj>=n||image[ni][nj]!=org||visited[ni][nj]) continue;
-                visited[ni][nj] = true;
-                int k = image[ni][nj];
-                image[ni][nj]=color;
-                q.push({ni,nj,k});
-            }
+        if (org == color)
+    return image;
+        queue<pair<int,int>> q;
+q.push({sr, sc});
+image[sr][sc] = color;
+ vector<vector<int>>dir = {{1,0},{-1,0},{0,1},{0,-1}};
+while (!q.empty()) {
+    auto [i, j] = q.front();
+    q.pop();
+
+    for (auto &d : dir) {
+        int ni = i + d[0];
+        int nj = j + d[1];
+
+        if (ni < 0 || nj < 0 || ni >= m || nj >= n)
+            continue;
+
+        if (image[ni][nj] == org) {
+            image[ni][nj] = color;
+            q.push({ni, nj});
         }
-        return image;
+    }
+}
+return image;
     }
 };
