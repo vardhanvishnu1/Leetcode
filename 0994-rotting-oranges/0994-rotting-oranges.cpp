690bc1297f8516f398b63fb36332396d59b153f7
vardhanvishnu1 <vardhanbharathula@gmail.com>
2026-07-19 00:04:19 +0530

    Time: 2 ms (31.5%), Space: 16.9 MB (46.5%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,39 +1,37 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
+        int m = grid.size();
+        int n = grid[0].size();
         queue<pair<int,int>>q;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==2){
-                    q.push({i,j});
-                }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==2) q.push({i,j});
             }
         }
-        vector<vector<int>>visited(n,vector<int>(m,0));
-        vector<pair<int,int>>dir = {{1,0},{-1,0},{0,-1},{0,1}};
-        int time =0;
+        int time = 0;
+        vector<vector<int>>dir = {{1,0},{-1,0},{0,1},{0,-1}};
         while(!q.empty()){
-            int size = q.size();bool pushed = false;
-            for(int r=0;r<size;r++){
-                auto [i,j] = q.front();q.pop();
+            int sz = q.size();
+            while(sz--){
+                int ci = q.front().first;
+                int cj = q.front().second;
+                q.pop();
+                if(grid[ci][cj]==0) continue;
                 for(int d=0;d<4;d++){
-                    int new_i = i + dir[d].first;
-                    int new_j = j + dir[d].second;
-                    if(new_i<0||new_j<0||new_i>=n||new_j>=m||grid[new_i][new_j]==0||visited[new_i][new_j]||grid[new_i][new_j]==2) continue;
-                    if(grid[new_i][new_j]==1){
-                        visited[new_i][new_j] = true;
-                        grid[new_i][new_j] = 2;
-                        pushed=true;
-                        q.push({new_i,new_j});
+                    int ni = ci + dir[d][0];
+                    int nj = cj + dir[d][1];
+                    if(ni<0||nj<0||ni>=m||nj>=n||grid[ni][nj]==0) continue;
+                    if(grid[ni][nj]==1){
+                        grid[ni][nj]=2;
+                        q.push({ni,nj});
                     }
                 }
             }
-            if(pushed) time++;
+            if(!q.empty()) time++;
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(grid[i][j]==1) return -1;
             }
         }
