f4531016f50f773c00d651ea691c8715e8b507ef
vardhanvishnu1 <vardhanbharathula@gmail.com>
2026-07-19 00:58:35 +0530

    Time: 0 ms (100%), Space: 18.9 MB (8.5%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,36 +1,29 @@
 class Solution {
 public:
-struct PairHash {
-        size_t operator()(const pair<int, int>& p) const {
-            return hash<int>()(p.first) ^ (hash<int>()(p.second) << 1);
-        }
-    };
-
-void solve(vector<vector<int>>& image, int sr, int sc, int color,int initial){
-    vector<vector<int>>dir = {{0,1},{0,-1},{1,0},{-1,0}};
-    queue<pair<int,int>>q;
-    unordered_set<pair<int,int>,PairHash>visited;
-    visited.insert({sr,sc});
-    q.push({sr,sc});
-    while(!q.empty()){
-        auto [curr_r,curr_c] = q.front();
-        q.pop();
-        image[curr_r][curr_c] = color;
-        for(int i=0;i<4;i++){
-            int new_r = curr_r + dir[i][0];
-            int new_c = curr_c + dir[i][1];
-            if(new_r<0||new_c<0||new_r>=image.size()||new_c>=image[0].size()) continue;
-            if(image[new_r][new_c]==initial&&visited.find({new_r,new_c})==visited.end()){
-                image[new_r][new_c] = color;
-                q.push({new_r,new_c});
-                visited.insert({new_r,new_c});
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int m = image.size();
+        int n = image[0].size();
+        int org = image[sr][sc];
+        queue<tuple<int,int,int>>q;
+        vector<vector<int>>dir = {{1,0},{-1,0},{0,1},{0,-1}};
+        q.push({sr,sc,org});
+        vector<vector<bool>>visited(m,vector<bool>(n,false));
+        visited[sr][sc] = true;
+        image[sr][sc] = color;
+        while(!q.empty()){
+            auto [ci,cj,x] = q.front();
+            org = x;
+            q.pop();
+            for(int d=0;d<4;d++){
+                int ni = ci + dir[d][0];
+                int nj = cj + dir[d][1];
+                if(ni<0||nj<0||ni>=m||nj>=n||image[ni][nj]!=org||visited[ni][nj]) continue;
+                visited[ni][nj] = true;
+                int k = image[ni][nj];
+                image[ni][nj]=color;
+                q.push({ni,nj,k});
             }
         }
-    }
-}
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        if(image[sr][sc]==color) return image; 
-        solve(image,sr,sc,color,image[sr][sc]);
         return image;
     }
 };
