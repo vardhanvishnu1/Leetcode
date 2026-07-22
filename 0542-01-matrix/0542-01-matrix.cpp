f63c69a9948f730e76afa57b298a80c180b352c6
vardhanvishnu1 <vardhanbharathula@gmail.com>
2026-07-22 12:09:41 +0530

    Time: 16 ms (66.78%), Space: 34.7 MB (87.24%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,30 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int m  = mat.size();
+        int m = mat.size();
         int n = mat[0].size();
+        vector<vector<int>>dist(m,vector<int>(n,0));
         queue<pair<int,int>>q;
-        vector<vector<int>>dist(m,vector<int>(n,-1));
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(mat[i][j]==0){
-                    dist[i][j] = 0;
                     q.push({i,j});
+                }
+                else{
+                    dist[i][j] = INT_MAX;
                 }
             }
         }
-
-    vector<vector<int>>dir = {{1,0},{-1,0},{0,1},{0,-1}};
-    while(!q.empty()){
-        auto [curr_i,curr_j] = q.front();q.pop();
-        for(int d = 0;d<4;d++){
-                int new_i = curr_i + dir[d][0];
-                int new_j = curr_j + dir[d][1];
-                if(new_i<0||new_j<0||new_i>=m||new_j>=n||dist[new_i][new_j]!=-1) continue;
-                dist[new_i][new_j] = dist[curr_i][curr_j] +1;
-                q.push({new_i,new_j});
+        vector<vector<int>>dir = {{1,0},{-1,0},{0,1},{0,-1}};
+        while(!q.empty()){
+            int ci = q.front().first;
+            int cj = q.front().second;
+            q.pop();
+            for(int d=0;d<4;d++){
+                int ni = ci + dir[d][0];
+                int nj = cj + dir[d][1];
+                if(ni<0||nj<0||ni>=m||nj>=n||mat[ni][nj]==0||dist[ni][nj]!=INT_MAX) continue;
+                dist[ni][nj]  = dist[ci][cj]+1;
+                q.push({ni,nj});
+            }
         }
-    }
     return dist;
     }
 };
