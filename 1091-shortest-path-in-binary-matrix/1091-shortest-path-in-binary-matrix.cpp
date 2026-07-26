6bb0c015244611f1364e83341de6c35c62d01092
vardhanvishnu1 <vardhanbharathula@gmail.com>
2026-07-26 15:28:30 +0530

    Time: 44 ms (16.87%), Space: 24.9 MB (65.05%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -3,32 +3,26 @@ public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n = grid.size();
         if(grid[0][0]==1||grid[n-1][n-1]==1) return -1;
-        vector<vector<int>>dir = {{0,1},{0,-1},{1,0},{-1,0},{1,1},{1,-1},{-1,-1},{-1,1}};
-        queue<pair<int,int>>q;
-        q.push({0,0});
-        int d = 1;
-        vector<vector<bool>>visited(n,vector<bool>(n,false));
-        visited[0][0] = true;
-        while(!q.empty()){
-            int sz = q.size();
-            while(sz>0){
-                auto [curr_i,curr_j] = q.front();q.pop();
-                if(curr_i==n-1&&curr_j==n-1) return d;
-                for(int i=0;i<8;i++){
-                    int new_i,new_j;
-                    new_i = curr_i+dir[i][0];
-                    new_j = curr_j+dir[i][1];
-                    if(new_i<0||new_j<0||new_i>=n||new_j>=n||grid[new_i][new_j]==1) continue;
-                    if(new_i==n-1&&new_j==n-1) return d+1;
-                    if(!visited[new_i][new_j]){
-                        visited[new_i][new_j] = true;
-                        q.push({new_i,new_j});
-                    }
+        vector<vector<int>>dist(n,vector<int>(n,INT_MAX));
+        priority_queue<tuple<int,int,int>,vector<tuple<int,int,int>>,greater<tuple<int,int,int>>>pq;
+        pq.push({0,0,0});
+        dist[0][0] = 0;
+        vector<vector<int>>dir = {{1,0},{-1,0},{0,1},{0,-1},{1,1},{-1,-1},{1,-1},{-1,1}};
+        while(!pq.empty()){
+            auto [dr,i,j] = pq.top();
+            pq.pop();
+            if(dr>dist[i][j]) continue;
+            for(int d=0;d<8;d++){
+                int ni = i + dir[d][0];
+                int nj = j + dir[d][1];
+                if(ni<0||nj<0||ni>=n||nj>=n||grid[ni][nj]==1) continue;
+                if(dr+1<dist[ni][nj]){
+                    dist[ni][nj] = dr+1;
+                    pq.push({dist[ni][nj],ni,nj});
                 }
-                sz--;
             }
-            d++;
         }
-        return -1;
+        if(dist[n-1][n-1]==INT_MAX) return -1;
+        return dist[n-1][n-1]+1;
     }
 };
